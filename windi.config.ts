f71d234e8b08df12f4eda5130383181e92410bca
Dave136 <davejs136@gmail.com>
2022-11-09 12:45:27 -0400

    :tada: initial configuration

---
+++ windi.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'windicss/helpers';
+
+export default defineConfig({
+  theme: {
+    colors: {
+      primary: 'cyan',
+      secondary: 'lightgreen',
+      third: 'lightgray',
+    },
+    textColor: {
+      primary: '#3490dc',
+      secondary: 'green',
+      info: 'blue',
+    },
+  },
+});
