a3561f1550ebd0cdb8cdf493a4bb81c24207a8f2
Fredy Polania <fpolania@gmail.com>
2022-12-30 14:49:19 -0400

    add: vue-skeletor plugin

--- pages/disciplinas/index.vue
+++ pages/disciplinas/index.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="swiper">
-    <swiper :slider-per-view="1" :space-between="0" loop navigation :modules="[Autoplay, Navigation, Pagination]">
+    <swiper
+      :slider-per-view="1"
+      :space-between="0"
+      loop
+      navigation
+      :modules="[Autoplay, Navigation, Pagination]"
+    >
       <swiper-slide v-for="(slide, index) in slides" :key="index">
-        <nuxt-img :src="slide.attributes.url" sizes="xs:100vw sm:640 md:768 lg:1024 xl:1280 xxl:1920" />
+        <nuxt-img
+          :src="slide.attributes.url"
+          sizes="xs:100vw sm:640 md:768 lg:1024 xl:1280 xxl:1920"
+        />
       </swiper-slide>
     </swiper>
   </div>
@@ -10,23 +19,39 @@
   <div class="disciplinas">
     <section class="disciplinas__container">
       <h2 class="disciplinas__title">Disciplinas</h2>
-      <div class="cards__wrapper">
+      <div
+        v-if="disciplinas !== undefined && disciplinas!"
+        class="cards__wrapper"
+      >
         <div v-for="(disciplina, index) in disciplinas" :key="index">
-          <nuxt-link :to="{ path: `/disciplinas/${disciplina.attributes.link}`, replace: true }">
-            <card :logo="disciplina.attributes.icono.data.attributes.url" :alternativeText="
-              disciplina.attributes.icono.data.attributes.alternativeText
-            " :title="disciplina.attributes.nombre" description="Ver información y planes" />
+          <nuxt-link
+            :to="{
+              path: `/disciplinas/${disciplina.attributes.link}`,
+              replace: true,
+            }"
+          >
+            <card
+              :logo="disciplina.attributes.icono.data.attributes.url"
+              :alternativeText="
+                disciplina.attributes.icono.data.attributes.alternativeText
+              "
+              :title="disciplina.attributes.nombre"
+              description="Ver información y planes"
+            />
           </nuxt-link>
         </div>
       </div>
+      <Skeletor v-else-if="isLoading === true" height="200" width="200" />
     </section>
   </div>
 
-  <highlight title="Nuestra piscina es un <br />
+  <highlight
+    title="Nuestra piscina es un <br />
         lugar de encuentro, es nuestra <br />
         casa, es CCS Multisport"
     description="Esta piscina alberga los sueños y las ilusiones de muchos y el escape de la rutina para otros.Aquí nadan los que entrenan duro, los que entrenan para competir, para relajarse y para llevar una una vida saludable"
-    image="https://res.cloudinary.com/novanet-studio/image/upload/v1669823171/ccs-multisport/cms_piscina_en_atardecer_281eb141f9.webp" />
+    image="https://res.cloudinary.com/novanet-studio/image/upload/v1669823171/ccs-multisport/cms_piscina_en_atardecer_281eb141f9.webp"
+  />
 </template>
 
 <script lang="ts" setup>
@@ -35,6 +60,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
+let isLoading: boolean = true;
 const slides = ref();
 const { disciplines: disciplinas } = useDisciplines();
 const graphql = useStrapiGraphQL();
