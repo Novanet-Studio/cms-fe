7747a3d6ca5453f189c2e8e7976296eef2076f9b
Fredy Polania <fpolania@gmail.com>
2023-04-25 14:47:16 -0400

    chore: no target config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@ import pwa from "./pwa";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  target: "static",
   ssr: true,
   telemetry: false,
   build: {
