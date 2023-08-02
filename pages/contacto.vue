b8d649b105756b82da9197f13b369591d8249a8f
Fredy Polania <fpolania@gmail.com>
2023-08-02 16:04:52 -0400

    add: static contact info

--- pages/contacto.vue
+++ pages/contacto.vue
@@ -1,13 +1,31 @@
 <template>
   <slider :slides="carrusel" />
   <section class="box">
-    <p class="accordion-item">
-      Te invitamos a visitar y conocer nuestra sede en la Alta Florida, una
-      piscina semi-olímpica, temperada, con un inmejorable equipo de trabajo
-      liderado por nuestro Coach certificado en entrenamiento para Triatlón y
-      con el mejor equipo de entrenadores capaces de brindar un ambiente cálido
-      y estimulante para todos.
-    </p>
+    <div class="summary-content">
+      <p class="accordion-item">
+        Te invitamos a visitar y conocer nuestra sede en la Alta Florida, una
+        piscina semi-olímpica, temperada, con un inmejorable equipo de trabajo
+        liderado por nuestro Coach certificado en entrenamiento para Triatlón y
+        con el mejor equipo de entrenadores capaces de brindar un ambiente
+        cálido y estimulante para todos.
+      </p>
+      <br/>
+      <h3>Horarios de atención</h3>
+      <p>Oficina Administrativa para pagos y formalizar inscripción</p>
+      <br/>
+      <h3>De Lunes a Viernes</h3>
+      <ul>
+        <li>De 9:00 am a 12:30pm</li>
+        <li>De 1:30 pm a 07:30pm</li>
+      </ul>
+      <br/>
+      <h3>Sábados</h3>
+   
+        <p>De 9:00 am A 12:00pm</p>
+        <br />
+        <h3>Teléfonos</h3>
+        <p>0212–935.45.46 / 935.45.47</p>
+    </div>
   </section>
   <div class="contacto__grid">
     <contact-form />
