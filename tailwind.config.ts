ba1425423b55425b3f0240e4aada2888850679fe
David Arenas <davejs136@gmail.com>
2024-06-26 00:18:16 -0400

    config: set tailwindcss plugins & variants

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,19 @@ import { iconsPlugin, getIconCollections } from "@egoist/tailwindcss-icons";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
+  },
   plugins: [
+    require("@tailwindcss/forms"),
     iconsPlugin({
       // Select the icon collections you want to use
       collections: getIconCollections(["fa-solid", "fa-brands"]),
