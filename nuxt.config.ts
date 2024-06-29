4ea837526bf13a209ef2cb1d16a106205995c46b
David Arenas <davejs136@gmail.com>
2024-06-28 20:23:35 -0400

    chore: remove packages & install `@nuxt/ui`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,12 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
-    "@nuxtjs/tailwindcss",
     "@nuxt/image",
     "@nuxtjs/google-fonts",
     "nuxt-gtag",
     "@nuxtjs/seo",
     "@nuxtjs/strapi",
+    "@nuxt/ui",
   ],
 
   googleFonts: {
