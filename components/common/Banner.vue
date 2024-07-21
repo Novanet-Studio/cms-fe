b933b687fe8f9f712bd07e99410e4935e0a4e601
David Arenas <davejs136@gmail.com>
2024-07-21 16:58:21 -0400

    style(home): banner responsive

--- components/common/Banner.vue
+++ components/common/Banner.vue
@@ -30,11 +30,13 @@ const props = withDefaults(defineProps<Props>(), {
 const classes = computed(() => {
   const container =
     props.contentPosition === "left"
-      ? "lg:grid-cols-[27rem_1fr] [&:first-child]:order-2"
-      : "lg:grid-cols-[1fr_27rem] [&:first-child]:rounded-r-lg";
+      ? "md:grid-cols-[18rem_1fr] lg:grid-cols-[27rem_1fr] [&:first-child]:order-2"
+      : "md:grid-cols-[1fr_18rem] lg:grid-cols-[1fr_27rem] [&:first-child]:rounded-r-lg";
 
   const asideCorner =
-    props.contentPosition === "left" ? "rounded-l-md" : "rounded-r-md";
+    props.contentPosition === "left"
+      ? "rounded-tr-md rounded-l-md md:rounded-tr-none"
+      : "rounded-bl-none rounded-r-md md:rounded-bl-none";
 
   const asideOrder = props.contentPosition === "left" ? "order-1" : "order-2";
 
@@ -46,7 +48,10 @@ const classes = computed(() => {
 </script>
 
 <template>
-  <section class="grid max-h-[33.75rem]" :class="classes.container">
+  <section
+    class="grid min-h-[17.81rem] md:min-h-[18.31] lg:max-h-[33.75rem]"
+    :class="classes.container"
+  >
     <div
       :class="
         contentPosition === 'left'
@@ -56,15 +61,21 @@ const classes = computed(() => {
     >
       <slot name="main" />
     </div>
-    <aside class="p-8 flex flex-col gap-4" :class="classes.aside">
+    <aside
+      class="p-8 md:p-4 md:gap-2 flex flex-col gap-4"
+      :class="classes.aside"
+    >
       <slot name="aside">
         <h2
-          class="font-black text-[37px] leading-[50px] text-pretty"
+          class="font-black text-pretty text-[30px] leading-tight md:text-lg lg:text-[37px] lg:leading-[50px]"
           :class="ui.aside.title"
         >
           {{ title }}
         </h2>
-        <p class="text-xl text-balance" :class="ui.aside.description">
+        <p
+          class="text-base text-balance md:text-sm lg:text-xl"
+          :class="ui.aside.description"
+        >
           {{ description }}
         </p>
       </slot>
--- components/home/Banner.vue
+++ components/home/Banner.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { useQuery } from "@tanstack/vue-query";
+
 const items = [
   "https://picsum.photos/1920/1080?random=1",
   "https://picsum.photos/1920/1080?random=2",
@@ -7,12 +9,83 @@ const items = [
   "https://picsum.photos/1920/1080?random=5",
   "https://picsum.photos/1920/1080?random=6",
 ];
+
+const STALE_TIME = 1000 * 60 * 15;
+
+const gql = useStrapiGraphQL();
+
+const fetchMain = gql<any>(`
+  query {
+    inicio {
+      data {
+        attributes {
+          principal {
+            titulo
+            descripcion
+            imagen {
+              data {
+                attributes {
+                  url
+                  alternativeText
+                }
+              }
+            }
+          }
+
+          mensaje {
+            titulo
+            descripcion
+          }
+        }
+      }
+    }
+
+    carrusel(id: 1) {
+      data {
+        attributes {
+          imagenes {
+            data {
+              attributes {
+                url
+                alternativeText
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`);
+
+const {
+  data: response,
+  suspense,
+  isLoading,
+} = useQuery({
+  queryKey: ["home-content"],
+  queryFn: () => fetchMain,
+  select({ data }) {
+    return {
+      main: data.inicio.data.attributes.principal,
+      carousel: data.carrusel.data.attributes.imagenes.data,
+    };
+  },
+  // 15 minutes
+  staleTime: STALE_TIME,
+});
+
+await suspense();
 </script>
 
 <template>
+  <USkeleton
+    class="w-full bg-gray-300 md:h-[18.31] lg:h-[540px]"
+    v-if="isLoading"
+  />
   <CommonBanner
-    title="Bienvenidos al Ccs MultiSport"
-    description="Somos un centro deportivo integral para todo público; cada deportista es un mundo diferente, atenderlo es el motivo que nos mueve a superarnos para ser cada día mejor."
+    v-else
+    :title="response?.main?.titulo || '-'"
+    :description="response?.main?.descripcion || '-'"
     content-position="left"
     :ui="{
       aside: {
@@ -25,11 +98,16 @@ const items = [
     <template #main>
       <UCarousel
         v-slot="{ item }"
-        :items="items"
+        :items="response?.carousel"
         :ui="{ item: 'basis-full' }"
-        class="overflow-hidden rounded-r-md"
+        class="overflow-hidden rounded-b-md min-h-[17.81rem] md:rounded-bl-none md:min-h-[18.31] lg:min-h-[540px] md:rounded-r-md"
       >
-        <img :src="item" class="w-full" draggable="false" />
+        <img
+          class="w-full object-cover min-h-[17.81rem] md:min-h-[18.31] lg:min-h-[540px]"
+          :src="item?.attributes?.url"
+          :alt="item?.attributes?.alternativeText"
+          :draggable="false"
+        />
       </UCarousel>
     </template>
   </CommonBanner>
