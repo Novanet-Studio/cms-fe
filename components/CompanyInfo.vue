0217280fd32aa9cba3d121f75f9246f8ea05b584
David Arenas <davejs136@gmail.com>
2024-07-21 23:33:32 -0400

    style(faq): responsive

---
+++ components/CompanyInfo.vue
@@ -0,0 +1,65 @@
+<template>
+  <aside class="flex flex-col gap-4">
+    <div class="contacto__group">
+      <h2 class="font-black text-xl text-[#231f20]">Ubicación</h2>
+      <p class="text-sm text-[#231f20]">
+        Urb. Alta Florida Av. Los Mangos Quinta Caracas Multisport (al frente de
+        la Plaza Benito Juárez subiendo a la Cota Mil).
+        <a
+          class="text-[var(--color-primary)]"
+          href="https://maps.app.goo.gl/X25KbsffHZEN5Y236"
+          target="_blank"
+        >
+          Abrir en Google Maps
+          <UIcon name="i-fa-solid-map" />
+          <!-- <font-awesome-icon :icon="['fas', 'map']" /> -->
+        </a>
+      </p>
+    </div>
+    <div class="contacto__group">
+      <h2 class="font-black text-xl text-[#231f20]">Información general</h2>
+      <p class="text-sm text-[#231f20]">info@ccsmultisport.com</p>
+    </div>
+
+    <div class="contacto__group">
+      <h2 class="font-black text-xl text-[#231f20]">Buzón de sugerencias</h2>
+      <p class="text-sm text-[#231f20]">sugerencias@ccsmultisport.com</p>
+    </div>
+    <div class="contacto__group">
+      <h2 class="font-black text-xl text-[#231f20]">Teléfonos</h2>
+      <p class="text-sm text-[#231f20]">+58 212-935.45.46 / 0212–935.45.46</p>
+    </div>
+    <div class="contacto__group">
+      <h2 class="font-black text-xl text-[#231f20]">WhatsApp</h2>
+      <div>
+        <a
+          class="text-[var(--color-primary)]"
+          href="https://wa.me/message/XTA7RUDHZKT4C1"
+          target="_blank"
+        >
+          <UIcon name="i-fa-brands-whatsapp" />
+        </a>
+      </div>
+    </div>
+
+    <div class="contacto__group">
+      <h2 class="font-black text-xl text-[#231f20]">Redes sociales</h2>
+      <div class="flex gap-2">
+        <a
+          class="text-[var(--color-primary)]"
+          href="https://www.facebook.com/CcsMultiSport"
+          target="_blank"
+        >
+          <UIcon name="i-fa-brands-facebook" />
+        </a>
+        <a
+          class="text-[var(--color-primary)]"
+          href="https://www.instagram.com/ccsmultisport/"
+          target="_blank"
+        >
+          <UIcon name="i-fa-brands-instagram" />
+        </a>
+      </div>
+    </div>
+  </aside>
+</template>
--- pages/preguntas-frecuentes.vue
+++ pages/preguntas-frecuentes.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
+import { useQuery } from "@tanstack/vue-query";
+
+const gql = useStrapiGraphQL();
+
 const items = [
-  "https://picsum.photos/1920/1080?random=1",
-  "https://picsum.photos/1920/1080?random=2",
-  "https://picsum.photos/1920/1080?random=3",
-  "https://picsum.photos/1920/1080?random=4",
-  "https://picsum.photos/1920/1080?random=5",
-  "https://picsum.photos/1920/1080?random=6",
+  "https://res.cloudinary.com/novanet-studio/image/upload/v1674752999/ccs-multisport/cms_triatleta_en_cada_disciplina_890ac7a154.webp",
 ];
 
 const accordionItems = [
@@ -28,6 +27,39 @@ const accordionItems = [
     slot: "rules",
   },
 ];
+
+const query = gql<any>(`
+    query {
+      preguntas(sort: "id:asc") {
+        data {
+          attributes {
+            titulo
+            descripcion
+          }
+        }
+      }
+    }
+  `);
+
+const {
+  data: response,
+  suspense,
+  isLoading,
+} = useQuery({
+  queryKey: ["faq"],
+  queryFn: () => query,
+  select({ data }) {
+    console.log("faq", data.preguntas.data);
+    return data.preguntas.data.map((faq) => ({
+      label: faq.attributes.titulo,
+      content: faq.attributes.descripcion,
+    }));
+  },
+  // 15 minutes
+  staleTime: 1000 * 60 * 15,
+});
+
+await suspense();
 </script>
 
 <template>
@@ -48,37 +80,48 @@ const accordionItems = [
           v-slot="{ item }"
           :items="items"
           :ui="{ item: 'basis-full' }"
-          class="overflow-hidden rounded-l-md"
+          class="overflow-hidden rounded-t-md md:rounded-l-md md:rounded-tr-none min-h-[17.81rem] md:min-h-[18.31] lg:max-h-[33.75rem]"
         >
-          <img :src="item" class="w-full" draggable="false" />
+          <img
+            :src="item"
+            class="w-full object-cover min-h-[17.81rem] md:min-h-[18.31] lg:min-h-[33.75rem]"
+            draggable="false"
+          />
         </UCarousel>
       </template>
     </CommonBanner>
 
-    <div class="mt-4 w-full">
+    <div class="mt-12 w-full lg:mb-32">
       <h3 class="font-black text-3xl">Preguntas frecuentes</h3>
 
-      <div class="grid lg:grid-cols-[1fr_25rem] lg:gap-8 mt-6">
-        <div>
+      <div
+        class="grid gap-8 md:grid-cols-[1fr_33%] lg:grid-cols-[1fr_25rem] lg:gap-8 mt-6"
+      >
+        <div class="ring-1 ring-offset-8 ring-[#E6E7E8] rounded-md">
           <UAccordion
             class="rounded-xl"
             color="transparent"
             size="xl"
-            :items="accordionItems"
+            :items="response"
             :ui="{
+              wrapper: 'gap-6',
               container:
-                'bg-gradient-to-r from-[#f3f3f4] to-[#E6E7E8] shadow-sm rounded-lg ring-1 ring-offset-8 ring-[#E6E7E8] border border-[#9E9E9E]',
+                'bg-gradient-to-r from-[#f3f3f4] to-[#E6E7E8] shadow-sm rounded-lg border border-[#9E9E9E]',
               item: {
-                base: 'text-[#001E61] font-black',
+                base: 'text-[#231F20] p-6',
               },
             }"
           >
             <template #rules="{ item }">
-              <div class="text-black" v-html="item.content"></div>
+              <div class="font-light text- p-4">
+                {{ item.content }}
+              </div>
             </template>
           </UAccordion>
         </div>
-        <div>Asde here...defineOptions</div>
+        <div>
+          <CompanyInfo />
+        </div>
       </div>
     </div>
   </section>
