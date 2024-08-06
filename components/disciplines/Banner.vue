56268348785e5fb3d9738db567587a8e904cc598
David Arenas <davejs136@gmail.com>
2024-08-05 23:47:46 -0400

    refactor: replace `NuxtImg` by `img`

--- components/disciplines/Banner.vue
+++ components/disciplines/Banner.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="h-[17.81rem] md:min-h-[18.31] lg:min-h-[540px]">
-    <NuxtImg
+    <img
       class="w-full h-[17.81rem] md:min-h-[18.31] lg:min-h-[540px] rounded-xl"
       src="https://res.cloudinary.com/novanet-studio/image/upload/v1679077756/ccs-multisport/cms_disciplinas_triatlon_7cef0ea59e.webp"
       loading="lazy"
