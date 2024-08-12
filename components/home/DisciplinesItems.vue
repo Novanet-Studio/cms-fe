94bbb6efafe31c15d4abb2493118ca179004ff9c
David Arenas <davejs136@gmail.com>
2024-08-11 21:39:31 -0400

    feat: disciplines click action

--- components/home/DisciplinesItems.vue
+++ components/home/DisciplinesItems.vue
@@ -48,7 +48,8 @@ await suspense();
       class="flex flex-col items-center gap-6"
     >
       <div
-        class="w-24 h-24 md:w-16 md:h-16 bg-[#f18a00] rounded-full flex justify-center items-center lg:w-24 lg:h-24"
+        class="w-24 h-24 md:w-16 md:h-16 bg-[#f18a00] rounded-full flex justify-center items-center lg:w-24 lg:h-24 cursor-pointer"
+        @click="$router.push(`/disciplinas/${discipline.attributes.link}`)"
       >
         <img
           class="w-12 md:w-10"
@@ -59,7 +60,7 @@ await suspense();
         />
       </div>
       <div class="text-[var(--color-tertiary)] gap-2 text-center">
-        <h4 class="font-black text-3xl md:text-xl lg:text-3xl">
+        <h4 class="font-black text-3xl md:text-xl lg:text-3xl cursor-pointer" @click="$router.push(`/disciplinas/${discipline.attributes.link}`)">
           {{ discipline.attributes.nombre }}
         </h4>
         <NuxtLink
