35c5ce70da805f9cc17940412638107df8320961
David Arenas <davejs136@gmail.com>
2024-09-19 18:10:30 -0400

    feat: add disciplines banner images

--- components/disciplines/Banner.vue
+++ components/disciplines/Banner.vue
@@ -1,10 +1,17 @@
+<script setup lang="ts">
+import cmsIndividualYoga from '~/assets/images/disciplines/cms-disciplinas-individuales-yoga.webp'
+// import cmsIndividualNatacion from '~/assets/images/disciplines/cms-disciplinas-individuales-natacion.webp'
+import cmsIndividualKarate from '~/assets/images/disciplines/cms-disciplinas-individuales-karate.webp'
+import cmsIndividualTriatlon from '~/assets/images/disciplines/cms-disciplinas-individuales-triatlon.webp'
+</script>
+
 <template>
-  <section class="h-[17.81rem] md:min-h-[18.31] lg:min-h-[540px]">
-    <img
-      class="w-full h-[17.81rem] md:min-h-[18.31] lg:min-h-[540px] rounded-xl"
-      src="https://res.cloudinary.com/novanet-studio/image/upload/v1679077756/ccs-multisport/cms_disciplinas_triatlon_7cef0ea59e.webp"
-      loading="lazy"
-      draggable="false"
-    /> 
+  <section>
+    <div class="grid grid-cols-2 md:grid-cols-4">
+      <div class="w-full h-[17.81rem] md:min-h-[18.31] lg:min-h-[540px] object-cover object-center bg-cover bg-center" :style="{ backgroundImage: `url(${cmsIndividualYoga})` }"></div>
+      <div class="w-full h-[17.81rem] md:min-h-[18.31] lg:min-h-[540px] object-cover object-center bg-cover bg-center" :style="{ backgroundImage: `url(${cmsIndividualYoga})` }"></div>
+      <div class="w-full h-[17.81rem] md:min-h-[18.31] lg:min-h-[540px] object-cover object-center bg-cover bg-center" :style="{ backgroundImage: `url(${cmsIndividualKarate})` }"></div>
+      <div class="w-full h-[17.81rem] md:min-h-[18.31] lg:min-h-[540px] object-cover object-center bg-cover bg-center" :style="{ backgroundImage: `url(${cmsIndividualTriatlon})` }"></div>
+    </div>
   </section>
 </template>
